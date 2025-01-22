076943fdecf8ca9ee0de7ed263ed68cc87fdc226
esionascimento <esionascimento@proton.me>
2025-01-22 10:08:06 -0400

    refactor: routes index import LayoutAuth

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,18 @@
 import { RouteObject } from "react-router-dom";
+
 import LoginPage from "../pages/login/Login";
+import { LayoutAuth } from "../layouts/auth/layout-auth";
 
 export const PUBLIC_ROUTES: RouteObject = {
   children: [
     {
-      index: true,
-      element: <LoginPage />,
+      element: <LayoutAuth />,
+      children: [
+        {
+          index: true,
+          element: <LoginPage />,
+        },
+      ],
     },
   ],
 };
